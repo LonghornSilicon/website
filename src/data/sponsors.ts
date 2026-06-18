5bfd7c3371204a8e927afe864b0291268e4f87e1
Alan <117928703+A14N77@users.noreply.github.com>
2026-06-17 18:14:44 -0700

    partners and logos

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -10,22 +10,7 @@ export type Sponsor =
     };
 
 export const sponsors: Sponsor[] = [
-  {
-    kind: "logo",
-    name: "Cadence",
-    src: "/sponsors/cadence.jpg",
-    width: 1024,
-    height: 256,
-    href: "https://www.cadence.com",
-  },
-  {
-    kind: "logo",
-    name: "ChipAgents",
-    src: "/sponsors/chipagents.png",
-    width: 460,
-    height: 122,
-    href: "https://chipagents.ai",
-  },
+  // Startups
   {
     kind: "logo",
     name: "Bronco AI",
@@ -36,11 +21,19 @@ export const sponsors: Sponsor[] = [
   },
   {
     kind: "logo",
-    name: "Laboratory for Computer Architecture",
-    src: "/sponsors/lca.jpg",
-    width: 425,
-    height: 80,
-    href: "https://lca.ece.utexas.edu",
+    name: "ChipAgents",
+    src: "/sponsors/chipagents.png",
+    width: 480,
+    height: 280,
+    href: "https://chipagents.ai",
+  },
+  {
+    kind: "logo",
+    name: "Etched",
+    src: "/sponsors/etched-ai.png",
+    width: 296,
+    height: 170,
+    href: "https://www.etched.com",
   },
   {
     kind: "logo",
@@ -49,5 +42,47 @@ export const sponsors: Sponsor[] = [
     width: 1254,
     height: 1254,
     href: "https://www.sigmanticai.com/",
+  },
+  // Established industry
+  {
+    kind: "logo",
+    name: "Cadence",
+    src: "/sponsors/cadence.png",
+    width: 1200,
+    height: 300,
+    href: "https://www.cadence.com",
+  },
+  {
+    kind: "logo",
+    name: "Synopsys",
+    src: "/sponsors/synopsys_logo.svg_.webp",
+    width: 1280,
+    height: 290,
+    href: "https://www.synopsys.com",
+  },
+  {
+    kind: "logo",
+    name: "Silicon Labs",
+    src: "/sponsors/Silicon_Labs_2015.svg.png",
+    width: 1280,
+    height: 659,
+    href: "https://www.silabs.com",
+  },
+  // Labs
+  {
+    kind: "logo",
+    name: "Laboratory for Computer Architecture",
+    src: "/sponsors/lca.jpg",
+    width: 425,
+    height: 80,
+    href: "https://lca.ece.utexas.edu",
+  },
+  {
+    kind: "logo",
+    name: "UTDA",
+    src: "/sponsors/UTDA.png",
+    width: 1064,
+    height: 230,
+    href: "https://www.cerc.utexas.edu/utda/",
   },
 ];
